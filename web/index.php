e25f71c601ee4a4c8e85a35fff9a040722cc81f4
tekimen <youkidearitai@gmail.com>
2019-10-08 01:16:23 +0900

    fix revert commit

---
+++ web/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$routes = require __DIR__ . '/../app/routes.php';
+
+$request_uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$not_found = function() {
+    return [404, ['Content-Type' => 'text/html'], "<h1>404 Not Found</h1>"];
+};
+
+$f = $routes[$request_uri] ?? $not_found;
+[$status, $headers, $body] = $f();
+http_response_code($status);
+foreach ($headers as $name => $h) {
+    header("{$name}: {$h}");
+}
+
+/*
+var_dump($_SERVER["REQUEST_URI"]);
+var_dump($request_uri);
+var_dump($_GET);
+ */
+echo $body;
